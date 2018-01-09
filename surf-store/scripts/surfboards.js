20e978c0cecf5bbffd301f4e58ef725989c97957
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2018-01-09 00:36:19 -0200

    initial frontend design

---
+++ surf-store/scripts/surfboards.js
@@ -0,0 +1,20 @@
+[
+    {
+        "name": "PRANCHA FM SURF EVOLUTION MAUI",
+        "image": "surf-prancha-fm-surf-evolution-maui.jpg",
+        "price": 1499.9
+        "size": 7.4
+    },
+    {
+        "name": "PRANCHA FM SURF FUNBOARD TEAHUPOO"
+        "image": "surf-prancha-fm-surf-funboard-teahupoo.jpg",
+        "price": 1200
+        "size": 7.2      
+    },
+    {
+        "name": "PRANCHA DE SURF LOST SUB SCORCHER"
+        "image": "lost-prancha-de-surf-lost-sub-scorcher.jpg",
+        "price": 1990
+        "size": 5.11
+    }
+]
