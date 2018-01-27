37f9fc5a041e66da27b587cbf1333b8e10f6b473
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2018-01-27 03:19:53 -0200

    adding project archetype

---
+++ surfwear-api/src/main/java/com/rramalho/surfstore/surfwearapi/SurfwearApiApplication.java
@@ -0,0 +1,12 @@
+package com.rramalho.surfstore.surfwearapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfwearApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfwearApiApplication.class, args);
+	}
+}
